07e5386208f3bd4d93f599a49a3fa3432c821ea2
wang xinyi <wxyakx@sina.com>
2019-12-19 13:10:20 +0800

    well-functioning multi-boxplot

--- Boxplot_3.R
+++ Boxplot_3.R
@@ -7,55 +7,60 @@ library(gridExtra)
 file_path<-choose.files()
 bxplot_df<-read.csv(file_path)
 
-# colnames(bxplot_df[c(5:24)])
+# colname <- colnames(bxplot_df[c(5:ncol(bxplot_df))])
 # Tg: bxplot_df[c(1:38),]
+Tg_subset_df <- bxplot_df[which(bxplot_df$genetype == 'Tg'),]
 # WT: bxplot_df[c(39:78),]
+WT_subset_df <- bxplot_df[which(bxplot_df$genetype == 'WT'),]
 # CocageWT: bxplot_df[c(79:105),]
+cocageWT_subset_df <- bxplot_df[which(bxplot_df$genetype == 'cocageWT'),]
 
-a<-ggplot(bxplot_df[c(79:105),],aes(x=months_when_sampling,y=Mon_Mφ,group=months_when_sampling))+
+graph_list <- list()
+graph_list<-append(graph_list,"3")
+
+###--code for single graph-###
+a<-ggplot(cocageWT_subset_df,aes(x=months_when_sampling,y=Mon_Mφ,group=months_when_sampling))+
     geom_boxplot()+
-    geom_dotplot(binaxis='y', stackdir='center',dotsize=0.6)
+  geom_dotplot(binaxis='y', binwidth = 0.2, stackdir='center',dotsize=0.4,color="black")+
+  stat_summary(fun.y=mean, geom="point", shape=20, size=6, color="red", fill="red")+
+  stat_summary(fun.y=mean, geom="line", size=1, color="red", aes(group=1))
 
-b<-ggplot(bxplot_df[c(79:105),],aes(x=months_when_sampling,y=Neutrophils,group=months_when_sampling))+
-  geom_boxplot()+
-  geom_dotplot(binaxis='y', stackdir='center',dotsize=0.6)
+a
+###----end------###
 
-c<-ggplot(bxplot_df[c(79:105),],aes(x=months_when_sampling,y=CD4_CD8_,group=months_when_sampling))+
-  geom_boxplot()+
-  geom_dotplot(binaxis='y', stackdir='center',dotsize=0.6)
 
-d<-ggplot(bxplot_df[c(79:105),],aes(x=months_when_sampling,y=CD8,group=months_when_sampling))+
-  geom_boxplot()+
-  geom_dotplot(binaxis='y', stackdir='center',dotsize=0.6)
+###-----code for Multi-BOXPLOT---###
 
-e<-ggplot(bxplot_df[c(79:105),],aes(x=months_when_sampling,y=Effector_CD8,group=months_when_sampling))+
-  geom_boxplot()+
-  geom_dotplot(binaxis='y', stackdir='center',dotsize=0.6)
+out <- NULL
 
-f<-ggplot(bxplot_df[c(79:105),],aes(x=months_when_sampling,y=Memory_CD8,group=months_when_sampling))+
-  geom_boxplot()+
-  geom_dotplot(binaxis='y', stackdir='center',dotsize=0.6)
+for (i in seq_along(colname)){
+  
+  #https://dodata.wordpress.com/2012/10/25/ggplot2-in-loops-and-multiple-plots/
+  #print(paste("current immune cells is ", colname[i]))
+    p<-ggplot(cocageWT_subset_df,aes_string(x="months_when_sampling", y=colname[i], group="months_when_sampling"))+
+    geom_boxplot()+
+    geom_dotplot(binaxis='y', binwidth = 0.2, stackdir='center',dotsize=0.4,color="black")+
+    stat_summary(fun.y=mean, geom="point", shape=20, size=6, color="red", fill="red")+
+    stat_summary(fun.y=mean, geom="line", size=1, color="red", aes(group=1))
+  
+  out[[i]] <- p
+}
 
-g<-ggplot(bxplot_df[c(79:105),],aes(x=months_when_sampling,y=Naive_CD8,group=months_when_sampling))+
-  geom_boxplot()+
-  geom_dotplot(binaxis='y', stackdir='center',dotsize=0.6)
+grid.arrange(out[[1]], out[[2]], out[[3]], 
+             out[[4]], out[[5]], out[[6]], 
+             out[[7]], out[[8]], out[[9]], 
+             ncol = 3, nrow = 3,
+             top = textGrob("cocageWT_subset,1-9",gp=gpar(fontsize=20,font=1)))
 
-h<-ggplot(bxplot_df[c(79:105),],aes(x=months_when_sampling,y=B,group=months_when_sampling))+
-  geom_boxplot()+
-  geom_dotplot(binaxis='y', stackdir='center',dotsize=0.6)
+#ggsave(filename=paste("Plot of Profit versus",colnames(movieSummary[i]),".pdf",sep=" "), plot=p)
 
-i<-ggplot(bxplot_df[c(79:105),],aes(x=months_when_sampling,y=DC,group=months_when_sampling))+
-  geom_boxplot()+
-  geom_dotplot(binaxis='y', stackdir='center',dotsize=0.6)
-
-grid.arrange(a,b,
-             nrow=1,ncol = 2)
+###-----END OF BOXPLOT---###
 
 bxplot_df
 bxplot_df[,c(-1,-2,-3,-4)]
 
 
-#####
+#####---PCA:
 
 library(ggfortify)
 

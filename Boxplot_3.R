23408c9710f5102c7516121c11e26250141fc2d6
wang xinyi <wxyakx@sina.com>
2019-12-19 13:41:14 +0800

    delete single plot

--- Boxplot_3.R
+++ Boxplot_3.R
@@ -1,47 +1,39 @@
+###-----code for Multi-BOXPLOT---###
+
+# 1.import library
 library(ggplot2)
 library(ggsignif)
 library(dplyr)
 library(grid)
 library(gridExtra)
 
+# 2.import files
 file_path<-choose.files()
 bxplot_df<-read.csv(file_path)
 
-# colname <- colnames(bxplot_df[c(5:ncol(bxplot_df))])
-# Tg: bxplot_df[c(1:38),]
+# 3.set variables
+n_col_value = 5
+
 Tg_subset_df <- bxplot_df[which(bxplot_df$genetype == 'Tg'),]
-# WT: bxplot_df[c(39:78),]
 WT_subset_df <- bxplot_df[which(bxplot_df$genetype == 'WT'),]
-# CocageWT: bxplot_df[c(79:105),]
 cocageWT_subset_df <- bxplot_df[which(bxplot_df$genetype == 'cocageWT'),]
 
-graph_list <- list()
-graph_list<-append(graph_list,"3")
+grid_graph_title = "cocageWT_subset,1-9"
 
-###--code for single graph-###
-a<-ggplot(cocageWT_subset_df,aes(x=months_when_sampling,y=Mon_Mφ,group=months_when_sampling))+
-    geom_boxplot()+
-  geom_dotplot(binaxis='y', binwidth = 0.2, stackdir='center',dotsize=0.4,color="black")+
-  stat_summary(fun.y=mean, geom="point", shape=20, size=6, color="red", fill="red")+
-  stat_summary(fun.y=mean, geom="line", size=1, color="red", aes(group=1))
-
-a
-###----end------###
-
-
-###-----code for Multi-BOXPLOT---###
+# 4.start ploting
+colname <- colnames(bxplot_df[c(n_col_value:ncol(bxplot_df))])
 
 out <- NULL
-
 for (i in seq_along(colname)){
   
   #https://dodata.wordpress.com/2012/10/25/ggplot2-in-loops-and-multiple-plots/
   #print(paste("current immune cells is ", colname[i]))
-    p<-ggplot(cocageWT_subset_df,aes_string(x="months_when_sampling", y=colname[i], group="months_when_sampling"))+
-    geom_boxplot()+
-    geom_dotplot(binaxis='y', binwidth = 0.2, stackdir='center',dotsize=0.4,color="black")+
-    stat_summary(fun.y=mean, geom="point", shape=20, size=6, color="red", fill="red")+
-    stat_summary(fun.y=mean, geom="line", size=1, color="red", aes(group=1))
+  p<-ggplot(cocageWT_subset_df,aes_string(x="months_when_sampling", y=colname[i], 
+                                          group="months_when_sampling"))+
+  geom_boxplot()+
+  geom_dotplot(binaxis='y', binwidth = 0.2, stackdir='center',dotsize=0.5,color="black")+
+  stat_summary(fun.y=mean, geom="point", shape=20, size=6, color="red", fill="red")+
+  stat_summary(fun.y=mean, geom="line", size=1, color="red", aes(group=1))
   
   out[[i]] <- p
 }
@@ -50,11 +42,29 @@ grid.arrange(out[[1]], out[[2]], out[[3]],
              out[[4]], out[[5]], out[[6]], 
              out[[7]], out[[8]], out[[9]], 
              ncol = 3, nrow = 3,
-             top = textGrob("cocageWT_subset,1-9",gp=gpar(fontsize=20,font=1)))
-
+             top = textGrob(grid_graph_title, gp=gpar(fontsize=20,font=1)))
+#dev.off()
 #ggsave(filename=paste("Plot of Profit versus",colnames(movieSummary[i]),".pdf",sep=" "), plot=p)
 
 ###-----END OF BOXPLOT---###
+
+
+###--code for single graph-###
+###---for debug###
+
+#a<-ggplot(cocageWT_subset_df,aes(x=months_when_sampling,y=NK,group=months_when_sampling))+
+#    geom_boxplot()+
+#  geom_dotplot(binaxis='y', binwidth = 0.2, stackdir='center',dotsize=0.5,color="black")+
+#  stat_summary(fun.y=mean, geom="point", shape=20, size=6, color="red", fill="red")+
+#  stat_summary(fun.y=mean, geom="line", size=1, color="red", aes(group=1))
+
+#a
+###----end------###
+
+
+
+
+
 
 bxplot_df
 bxplot_df[,c(-1,-2,-3,-4)]

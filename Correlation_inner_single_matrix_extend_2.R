01924a4742f087039a003836c987c60cac6952bc
wang xinyi <wxyakx@sina.com>
2019-12-20 15:58:30 +0800

    Update Correlation_inner_single_matrix_extend_2.R

--- Correlation_inner_single_matrix_extend_2.R
+++ Correlation_inner_single_matrix_extend_2.R
@@ -1,10 +1,19 @@
 #import libarary
 #install.packages("ggcorrplot")
+if (!require(reshape2)){
+  install.packages('reshape2')
+  library(reshape2)
+}
+
 library(ggcorrplot)
+library(grid)
+library(gridExtra)
 
 #data type:
+#D:\971晚期菌群文章\肠免疫细胞\correlation\aggregated_for_one_step_correlation-191220.csv
 
-#month	group	NK	NKT
+#*don't change*
+#*month*	*group*	NK	NKT
 #7	cocageWT	0.2725	0.63625
 #9	cocageWT	0.3675	0.58125
 #11	cocageWT	1.318	1.604
@@ -21,23 +30,10 @@ library(ggcorrplot)
 #12	WT	1.353333333	3.025
 #16	WT	0.854	1.304
 
-
-
 #-------------------------INPUT-------
-#set title and font size:
-title="NK"
-
-#col number of unique_sample_name, 1-index. default = 1.
-col_usample=1
-
 x_label_size=8
 y_label_size=8
-value_label_size=1.5
-
-#output file name:
-file_path="C:\\Users\\wangxinyi\\Desktop\\temp\\"
-matrix_file_name=paste(file_path,"correlation matrix_",title,".csv",sep="")
-pvalue_file_name=paste(file_path,"pvalue_",title,".csv",sep="")
+value_label_size=3
 #------------------------------------
 
 
@@ -46,53 +42,74 @@ pvalue_file_name=paste(file_path,"pvalue_",title,".csv",sep="")
 file_directory<-choose.files(caption="choose aggregated .csv matrix")
 cor_df<-read.csv(file_directory)
 
+#loop through colnames of different cells
+out <- NULL
+colname<-colnames(cor_df)[3:ncol(cor_df)]
 
-if (!require(reshape2)){
-  install.packages('reshape2')
-  library(reshape2)
+for (i in seq_along(colname)){
+  
+  title=colname[i]
+  
+  # convert long format to wide format
+  cell = dcast(cor_df,month~group,value.var = colname[i])
+  selected_cell<-cell[,c(2:ncol(cell))]
+  
+  #ref
+  #https://www.listendata.com/2016/01/transpose-data-in-r.html
+  
+  #start calculate correlation
+  corr<-cor(selected_cell)
+  p.mat<-cor_pmat(selected_cell)
+  
+  ##WRITE TO CSV
+  #output file name:
+  file_path="C:\\Users\\wangxinyi\\Desktop\\temp\\"
+  matrix_file_name=paste(file_path,"correlation matrix_",title,".csv",sep="")
+  pvalue_file_name=paste(file_path,"pvalue_",title,".csv",sep="")
+  
+  #write correlation matrix:
+  write.csv(corr,matrix_file_name)
+  
+  #write pvalue out:
+  write.csv(p.mat,pvalue_file_name)
+  ###
+  
+  #plot
+  cor_plot_with_pvalue<-ggcorrplot(corr,hc.order=FALSE,type="lower",lab=TRUE,p.mat= p.mat, insig = "blank",lab_size=value_label_size)+
+    labs(title=title)+
+    theme(axis.text.x = element_text(size=x_label_size),axis.text.y = element_text(size=y_label_size))
+  
+  cor_plot_with_pvalue
+  
+  ##plot w/o pvalue:
+  cor_plot_without_pvalue<-ggcorrplot(corr,hc.order=FALSE,type="lower", lab=TRUE,lab_size=value_label_size)+
+    labs(title=title)+
+    theme(axis.text.x = element_text(size=x_label_size),axis.text.y = element_text(size=y_label_size))
+  
+  cor_plot_without_pvalue
+  
+
+  
+  out[[i]] <- grid.arrange(cor_plot_with_pvalue,cor_plot_without_pvalue,
+                           nrow=1,ncol = 2)
+  
 }
 
-# convert long format to wide format
-mydt = dcast(cor_df,month~group,value.var = "NK")
+##GROUP 1 PLOTING...
+tp<-grid.arrange(out[[1]], out[[2]], out[[3]], 
+             ncol = 1, nrow = 3,
+             top = textGrob("correlation", gp=gpar(fontsize=15,font=1)))
+
+ggsave("myuplot.pdf",path="C:\\Users\\wangxinyi\\Desktop\\temp\\")
 
 
-
-#ref
-#https://www.listendata.com/2016/01/transpose-data-in-r.html
-
-
-#start calculate correlation
-corr<-cor(cor_df)
-p.mat<-cor_pmat(cor_df)
-
-##WRITE TO CSV
-#write correlation matrix:
-write.csv(corr,matrix_file_name)
-
-#write pvalue out:
-write.csv(p.mat,pvalue_file_name)
-###
-
-#plot
-cor_plot_with_pvalue<-ggcorrplot(corr,hc.order=FALSE,type="lower",lab=TRUE,p.mat= p.mat, insig = "blank",lab_size=value_label_size)+
-  labs(title=title)+
-  theme(axis.text.x = element_text(size=x_label_size),axis.text.y = element_text(size=y_label_size))
-
-cor_plot_with_pvalue
-
-##plot w/o pvalue:
-cor_plot_without_pvalue<-ggcorrplot(corr,hc.order=FALSE,type="lower", lab=TRUE,lab_size=value_label_size)+
-  labs(title=title)+
-  theme(axis.text.x = element_text(size=x_label_size),axis.text.y = element_text(size=y_label_size))
-
-cor_plot_without_pvalue
-
-#arrange figures:
-library(grid)
-library(gridExtra)
-grid.arrange(cor_plot_with_pvalue,cor_plot_without_pvalue,
-             nrow=1,ncol = 2)
-
+##BATCH PLOTING...
+for (i in seq(1,19,by=3)){
+  output_p<-grid.arrange(out[[i]], out[[i+1]], out[[i+2]], 
+               ncol = 1, nrow = 3,
+               top = textGrob("correlation", gp=gpar(fontsize=15,font=1)))
+  ggsave(output_p,device="pdf",path="C:\\Users\\wangxinyi\\Desktop\\temp\\")
+}
 #end of code
 
 #ref:
